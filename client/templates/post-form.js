9f25ca4128bf56bc7a847da6db7a1e6ab6545368
Takahiro Yaota <zakuro@yuzakuro.me>
2015-11-23 22:43:58 +0900

    Add form

---
+++ client/templates/post-form.js
@@ -0,0 +1,20 @@
+Template.postForm.events({
+  'submit #post-form': (event, template) => {
+    event.preventDefault()
+
+    const $errors = template.$('#post-form__errors')
+    $errors.empty()
+
+    const $body = template.$('#post-form__body')
+    const $name = template.$('#post-form__name')
+    const roomId = Router.current().params.id
+
+    if ($body.val().length === 0 || $name.val().length === 0) {
+      $errors.append('<p>error</p>')
+      return
+    }
+
+    Comments.insert({roomId, body: $body.val(), name: $name.val()})
+    $body.val('')
+  },
+})
