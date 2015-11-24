e6bc8696df1dbe8e3a2063db9143f00c1e94cf71
Takahiro Yaota <zakuro@yuzakuro.me>
2015-11-24 15:17:08 +0900

    Add create-room-form

--- client/templates/root.js
+++ client/templates/root.js
@@ -1,2 +1,8 @@
 Template.root.events({
+  'submit #create-room-form': (event) => {
+    event.preventDefault()
+    Meteor.call('createRoom', (e, id) => {
+      Router.go(`/${id}`)
+    })
+  },
 })
